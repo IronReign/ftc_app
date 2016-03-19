770f05f3c6f123e7185a3d59f48d52508d926623
Iron Reign <IronReignRobotics@users.noreply.github.com>
2016-03-19 17:44:04 -0500

    CsvLogKeeper V2
    Can handle ArrayLists of variables now.

--- IronReign/src/main/java/com/ironreignrobotics/resq/CsvLogKeeper.java
+++ IronReign/src/main/java/com/ironreignrobotics/resq/CsvLogKeeper.java
@@ -7,6 +7,7 @@ package com.ironreignrobotics.resq;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class CsvLogKeeper{
 
@@ -15,9 +16,11 @@ public class CsvLogKeeper{
     private static String fileName;
     private static boolean alreadyExists;
     private static File logFile;
+    private static int dataLength;
 
     public CsvLogKeeper(String varname){
 
+        dataLength = 1;
         variableName = varname;
         fileName = varname+".csv";
         alreadyExists = new File(fileName).exists();
@@ -35,9 +38,44 @@ public class CsvLogKeeper{
         }
     }
 
+    public CsvLogKeeper(String title, int numvars, String varnames){
+
+        dataLength = numvars;
+        variableName = title;
+        fileName = title+".csv";
+        alreadyExists = new File(fileName).exists();
+        logFile = new File(fileName);
+        try{
+            logKeeper = new FileWriter(logFile, true);
+            if (!alreadyExists){
+                logKeeper.append("Log of variables "+variableName);
+                logKeeper.append("\n");
+                logKeeper.append(varnames);
+                logKeeper.append("\n");
+                logKeeper.flush();
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
+    public static void UpdateLog(ArrayList data){
+        try{
+            for(int i=0; i<dataLength; i++){
+                logKeeper.append(data.get(i)+",");
+            }
+            logKeeper.append("\n");
+            logKeeper.flush();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+
     public static void UpdateLog(double data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -47,7 +85,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(int data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -57,7 +95,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(short data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -67,7 +105,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(String data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -77,7 +115,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(long data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -87,7 +125,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(float data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -97,7 +135,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(char data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -107,7 +145,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(boolean data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -117,7 +155,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(byte data){
         try{
-            logKeeper.append(data+",");
+            logKeeper.append(data+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
@@ -127,7 +165,7 @@ public class CsvLogKeeper{
 
     public static void UpdateLog(Object data){
         try{
-            logKeeper.append(data.toString()+",");
+            logKeeper.append(data.toString()+"\n");
             logKeeper.flush();
         }
         catch(IOException e){
